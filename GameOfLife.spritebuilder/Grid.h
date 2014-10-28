8ab3b437e94afe73ea7bca4f110982723c0f06b9
Alexander Buessing <Alex@Alexanders-MBP.hsd1.ct.comcast.net>
2014-10-27 20:48:02 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,16 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Alexander Buessing on 10/25/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@property (nonatomic, assign) int totalAllive;
+@property (nonatomic, assign) int generation;
+
+@end
